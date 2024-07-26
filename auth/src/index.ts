e3396acb3f75080c4b06c84aa883d2a160e4abe3
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-07-26 10:12:57 +0530

    :hammer: logging for load testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import crypto from "node:crypto";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
+const serverHash = crypto.randomUUID().split("-").at(-1);
+
+let requestIndex = 0;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  requestIndex += 1;
+  console.log("Handled request ", requestIndex);
+  res.send(`Hello World! from ${serverHash}`);
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  console.log(`Server ${serverHash} listening on port ${port}`);
 });
